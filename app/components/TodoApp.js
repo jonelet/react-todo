092de969fbb70746cfbb8f9308f5d0e877ce13d9
jonelet <jonelet@gmail.com>
2016-11-01 14:18:52 +0100

    Add filter functionality

--- app/components/TodoApp.js
+++ app/components/TodoApp.js
@@ -1,37 +1,23 @@
 var React = require('react');
+var uuid = require('node-uuid');
+
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch  = require('TodoSearch');
-var uuid = require('node-uuid');
+var TodoAPI = require('todoAPI');
+
+
 
 var TodoApp = React.createClass({
   getInitialState: function () {
     return {
       showCompleted: false,
       searchText: '',
-      todos: [
-        {
-          id: uuid(),
-          text: 'Walk the dog',
-          completed: false
-        },
-        {
-          id: uuid(),
-          text: 'Clean the yeard',
-          completed: true
-        },
-        {
-          id: uuid(),
-          text: 'Playing with the cat',
-          completed: true
-        },
-        {
-          id: uuid(),
-          text: 'Feeding the cat',
-          completed: false
-        }
-      ]
-    }
+      todos: TodoAPI.getTodos()
+    };
+  },
+  componentDidUpdate: function () {
+    TodoAPI.setTodos(this.state.todos);
   },
   handleAddTodo: function (text) {
     this.setState({
@@ -62,12 +48,13 @@ var TodoApp = React.createClass({
     });
   },
   render: function () {
-    var {todos} = this.state;
+    var {todos, showCompleted, searchText} = this.state;
+    var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
 
     return  (
       <div>
         <TodoSearch onSearch={this.handleSearch}/>
-        <TodoList todos={todos} onToggle={this.handleToggle} />
+        <TodoList todos={filteredTodos} onToggle={this.handleToggle} />
         <AddTodo onAddTodo={this.handleAddTodo}/>
       </div>
     )
